01361a3efef50b444f515e33a1299503d49675b1
XShine555 <xshine555@gmail.com>
2026-03-30 15:55:44 +0200

    Missing configuration DI

---
+++ Application/Configuration/ConfigurationDependencyInjection.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Musify.Application.Configuration
+{
+    public static class ConfigurationDependencyInjection
+    {
+        public static IServiceCollection AddApplicationStorageConfiguration(this IServiceCollection serviceDescriptors, IConfiguration configuration)
+        {
+            serviceDescriptors
+                .AddOptionsWithValidateOnStart<ApplicationStorageConfiguration>()
+                .Bind(configuration.GetRequiredSection(ApplicationStorageConfiguration.SectionName))
+                .ValidateDataAnnotations();
+            serviceDescriptors.AddSingleton(serviceProvider =>
+                serviceProvider.GetRequiredService<IOptions<ApplicationStorageConfiguration>>().Value);
+
+            return serviceDescriptors;
+        }
+
+        public static IServiceCollection AddPlayListConfiguration(this IServiceCollection serviceDescriptors, IConfiguration configuration)
+        {
+            serviceDescriptors
+                .AddOptionsWithValidateOnStart<PlayListConfiguration>()
+                .Bind(configuration.GetRequiredSection(PlayListConfiguration.SectionName))
+                .ValidateDataAnnotations();
+            serviceDescriptors.AddSingleton(serviceProvider =>
+                serviceProvider.GetRequiredService<IOptions<PlayListConfiguration>>().Value);
+
+            return serviceDescriptors;
+        }
+
+        public static IServiceCollection AddTrackConfiguration(this IServiceCollection serviceDescriptors, IConfiguration configuration)
+        {
+            serviceDescriptors
+                .AddOptionsWithValidateOnStart<TrackConfiguration>()
+                .Bind(configuration.GetRequiredSection(TrackConfiguration.SectionName))
+                .ValidateDataAnnotations();
+            serviceDescriptors.AddSingleton(serviceProvider =>
+                serviceProvider.GetRequiredService<IOptions<TrackConfiguration>>().Value);
+
+            return serviceDescriptors;
+        }
+    }
+}
