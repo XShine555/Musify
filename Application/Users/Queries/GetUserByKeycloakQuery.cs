9000e563c43a74ec80eca041a73d6c619743aa25
XShine555 <xshine555@gmail.com>
2026-05-28 23:16:35 +0200

    Remove keycloak

--- Application/Users/Queries/GetUserByKeycloakQuery.cs
+++
@@ -1,9 +0,0 @@
-using Ardalis.Result;
-using Mediator;
-using Musify.Application.Users.Responses;
-
-namespace Musify.Application.Users.Queries
-{
-    public record GetUserByKeycloakQuery(Guid Id)
-        : IQuery<Result<UserApplicationResponse> >;
-}
