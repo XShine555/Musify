43c5061fac6b73a40048d540df7d485ca63d1249
XShine555 <xshine555@gmail.com>
2026-06-03 00:32:37 +0200

    Adjust

--- Application/PlayLists/Queries/GetPlayListsByUserIdQuery.cs
+++ Application/PlayLists/Queries/GetPlayListsByUserIdQuery.cs
@@ -7,7 +7,7 @@ namespace Musify.Application.PlayLists.Queries
 {
     public record GetPlayListsByUserIdQuery(
         long UserId,
-    string? Name = null,
+        string? Name,
         int PageNumber = 1,
         int PageSize = 10)
         : IQuery<Result<PaginatedResponse<PlayListApplicationResponse>> >;
