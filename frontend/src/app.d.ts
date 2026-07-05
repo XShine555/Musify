e11db5263b4585b0adb5a9c5fe52b4b36b481a75
XShine555 <xshine555@gmail.com>
2026-07-05 23:41:42 +0200

    chore(frontend): remove scaffold comments and add conventions doc

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,17 +1,11 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
 import type { SessionUser } from '$lib/types';
 
 declare global {
 	namespace App {
-		// interface Error {}
 		interface Locals {
 			user: SessionUser | null;
 			accessToken: string | null;
 		}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
 	}
 }
 
